234b6f78b19d4826cc8bb94136d2d0b66e2e634e
Erenthos <erenthos@gmail.com>
2025-11-14 11:58:41 +0530

    Create page.tsx

---
+++ src/app/dashboard/vendors/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function VendorsPage() {
+  return (
+    <div className="text-white p-10">
+      <h1 className="text-3xl font-bold">Vendor Management</h1>
+      <p>This page will show vendor add/delete list.</p>
+    </div>
+  );
+}
